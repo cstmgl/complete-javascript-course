86a984342e4735715e52e21d933bdb2481848599
Miguel Costa <Miguel.Costa@avaloq.com>
2019-09-13 15:44:44 +0800

    update to lesson 139. request for data does not work because request is blocked

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,28 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+//import str from './models/Search';
+
+//import {add, multiply, ID} from './views/searchView';
+//import {add as a , multiply as m, ID} from './views/searchView';
+//import * as searchView from './views/searchView';
+
+//console.log(`Using imported functions! ${add(ID, 2)} and ${multiply(3, 5)}. ${str}`);
+//console.log(`Using imported functions! ${a(ID, 2)} and ${m(3, 5)}. ${str}`);
+//console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}`);
+
+// ACTUAL APPLICATION CODE
+
+//API key 14377cc1984efd0e7fa70ebf931184e7
+//https://www.food2fork.com/api/search
+//https://www.food2fork.com/api/get
+//crossorigin.me
+
+import axios from 'axios';
+
+const crossOriginProxy = 'https://crossorigin.me';
+const herokuappProxy = 'https://cors-anywhere.herokuapp.com/';
+const apiKey = '14377cc1984efd0e7fa70ebf931184e7';
+
+async function getResults(query) {
+  const res = await axios(`${herokuappProxy}/https://www.food2fork.com/api/search?key=${apiKey}&q=${query}`);
+  console.log(res);
+}
+getResults('piza');
