46a8f1b187860440ec80f55ff9956bd876f8af21
Miguel Costa <cst.mgl@gmail.com>
2019-09-16 11:38:04 +0800

    lesson 144 end of left search

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -15,14 +15,65 @@
 //https://www.food2fork.com/api/get
 //crossorigin.me
 
-import axios from 'axios';
+// import axios from 'axios';
 
-const crossOriginProxy = 'https://crossorigin.me';
-const herokuappProxy = 'https://cors-anywhere.herokuapp.com/';
-const apiKey = '14377cc1984efd0e7fa70ebf931184e7';
+// const crossOriginProxy = 'https://crossorigin.me';
+// const herokuappProxy = 'https://cors-anywhere.herokuapp.com/';
+// const apiKey = '14377cc1984efd0e7fa70ebf931184e7';
 
-async function getResults(query) {
-  const res = await axios(`${herokuappProxy}/https://www.food2fork.com/api/search?key=${apiKey}&q=${query}`);
-  console.log(res);
-}
-getResults('piza');
+// async function getResults(query) {
+//   try {
+//     const res = await axios(`${herokuappProxy}https://www.food2fork.com/api/search?key=${apiKey}&q=${query}`);
+//     const recipes = res.data.recipes;
+//     console.log(recipes);
+//   } catch (error) {
+//     alert(error);
+//   }
+// }
+// getResults('tomato pasta');
+
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
+
+// global state of the application
+// - search object
+// - current recipe object
+// - shopping list object
+// - liked recipes
+const state = {};
+
+const controlSearch = async() => {
+  //1 get query from view
+  const query = searchView.getInput(); // get from view
+
+  if (query) {
+    //2 new search object and add to state
+    state.search = new Search(query);
+
+    //3a prepare UI for results
+    searchView.clearInput();
+
+    //3b clear UI results
+    searchView.clearResults();
+
+    //3c activate loader widget
+    renderLoader(elements.searchRes);
+
+    //4a preform search
+    await state.search.getResults();
+    //4b stop loader
+    clearLoader(elements.searchRes);
+
+
+    //5 render results on UI
+    searchView.renderResults(state.search.results);
+
+  }
+};
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
+
--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -1,1 +1,23 @@
-export default 'I am an exported string.';
+//export default 'I am an exported string.';
+
+import axios from 'axios';
+
+export default class Search {
+
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const herokuappProxy = 'https://cors-anywhere.herokuapp.com/';
+        const apiKey = '14377cc1984efd0e7fa70ebf931184e7';
+        
+        try {
+          const res = await axios(`${herokuappProxy}https://www.food2fork.com/api/search?key=${apiKey}&q=${this.query}`);
+          this.results = res.data.recipes;
+//          console.log(this.results);
+        } catch (error) {
+          alert(error);
+        }
+    }
+}
